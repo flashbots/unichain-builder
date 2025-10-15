5c35e5ceee7ec1cbfe46ba0cf1ec5396f0ce05c4
Ash Kunda <18058966+akundaz@users.noreply.github.com>
2025-10-15 15:21:35 -0400

    add step to add the builder tx

--- src/main.rs
+++ src/main.rs
@@ -147,12 +147,23 @@ fn build_flashblocks_pipeline(
 	// in limits
 	let max_flashblocks = Arc::new(AtomicU64::new(0));
 
-	let flashblock_building_pipeline = (
+	let flashblock_building_pipeline_steps = (
 		AppendOrders::from_pool(pool).with_ok_on_limit(),
 		OrderByPriorityFee::default(),
 		RemoveRevertedTransactions::default(),
 		BreakAfterDeadline,
 	)
+		.into_pipeline();
+
+	let flashblock_building_pipeline_steps =
+		if let Some(ref signer) = cli_args.builder_signer {
+			flashblock_building_pipeline_steps
+				.with_epilogue(BuilderEpilogue::with_signer(signer.clone().into()))
+		} else {
+			flashblock_building_pipeline_steps
+		};
+
+	let flashblock_building_pipeline = flashblock_building_pipeline_steps
 		.with_epilogue(PublishFlashblock::new(
 			&ws,
 			cli_args.flashblocks_args.calculate_state_root,
